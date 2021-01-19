80177a6979b8aaa89e62770350bc0efa5ece49db
Hisashi Shimoji <hshimoji1@mac.com>
2021-01-19 09:49:10 +0900

    debug

--- src/components/geo_view.js
+++ src/components/geo_view.js
@@ -72,7 +72,7 @@ const GeoView = (props) => {
   useEffect(() => {
     // Parse.initialize(process.env.B4A_APP_ID, process.env.B4A_JS_KEY);
     // Parse.serverURL = "https://parseapi.back4app.com/";
-    console.log("p_init " + Parse.serverURL)
+    console.log("p_init " + process.env.B4A_SERVER_URL)
   }, []);
 
   useEffect(() => {
